9e7d5647bcf0f314da3caaa533a853d23a0f172c
0639Quercus <thsomsits.brie@gmail.com>
2021-12-25 19:29:00 +0700

    lab14_1.cpp completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,29 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double data[], int N, double rtn[]){
+    double ari = 0, geo = 1, har = 0, max = data[0], min = data[0];
+    for(int i = 0; i < N; i++){
+        max = data[i] > max ? data[i] : max;
+        min = data[i] < min ? data[i] : min;
+        ari += data[i];
+        geo *= data[i];
+        har += 1/data[i];
+    }
+
+    ari = ari/N;
+    geo = pow(geo, 1./N);
+    har = N/har;
+
+    double sd = 0;
+    for(int i = 0; i < N; i++) sd += pow((data[i]-ari),2);
+    sd = sqrt(sd/N);
+
+    rtn[0] = ari;
+    rtn[1] = sd;
+    rtn[2] = geo;
+    rtn[3] = har;
+    rtn[4] = max;
+    rtn[5] = min;
+}
